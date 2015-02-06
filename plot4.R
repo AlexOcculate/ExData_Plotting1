01f48461351564d4aa8cf4c964605244ef388a22
AlexOcculate <occulate@yahoo.com>
2015-02-07 10:33:11 +1100

    Fixing comments...

--- plot4.R
+++ plot4.R
@@ -1,13 +1,12 @@
 rm( list=ls() )
-dn <- "C:/Users/Alex/Documents/Coursera/Johns Hopkins - Certification/Data Science/04 - Exploratory Data Analysis/data"
-# dn <- "~/Code/R/ExData_Plotting1"
-setwd( dn )
 #
 get.and.clean.household.power.consumption <- function() {
-   fn <- "household_power_consumption.txt"
-   hpc <- read.csv2( fn , dec=".", na.strings=c("","?","NA") , stringsAsFactors=FALSE ) 
+   hpc <- read.csv2( "data/household_power_consumption.txt" , dec=".", na.strings=c("","?","NA") , stringsAsFactors=FALSE ) 
+   # cast '$Date' to R-Date...
    hpc$Date <- as.Date( hpc$Date , "%e/%m/%Y" )
+   # subset the data for specific range... 
    hpc <- hpc[ hpc$Date >= as.Date( "2007-02-01" ) & hpc$Date <= as.Date( "2007-02-02" ) , ]
+   # create '$TimeStamp' for posterior calculations...
    hpc$TimeStamp <- as.POSIXct( strptime( paste( hpc$Date , hpc$Time ) , "%Y-%m-%e %H:%M:%S" ) )
    hpc
 }
